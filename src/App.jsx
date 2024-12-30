ad451a3b1386bdf5394ec2fc9efe2cf35fa06d40
masaud04 <ahmedmasaud942@gmail.com>
2024-12-30 13:07:36 +0600

    final commiit

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
     <div className="relative h-screen pt-32">
       <QrCodeGenerate />
 
-      <div className="absolute bottom-32 w-full">
+      {/* <div className="absolute bottom-32 w-full">
         <h2 className="author text-2xl md:text-4xl">
           &lt;<span className="text-gradient">masaudAhmod</span> /&gt;
         </h2>
-      </div>
+      </div> */}
     </div>
   );
 }
